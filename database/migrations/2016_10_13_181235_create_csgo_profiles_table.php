4a381b41a5ec9eb388788b6bb01cc69f5255fe2d
Exitialis <lpexitialis@gmail.com>
2016-10-13 21:21:08 +0300

    Добавлены таблицы в базу

---
+++ database/migrations/2016_10_13_181235_create_csgo_profiles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCsgoProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('csgo_profiles', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('kills')->unsigned();
+            $table->integer('deaths')->unsigned();
+            $table->integer('time_played')->unsigned();
+            $table->integer('wins')->unsigned();
+            $table->integer('kills_headshot')->unsigned();
+            $table->integer('shots_fired')->unsigned();
+            $table->integer('shots_hit')->unsigned();
+            $table->integer('shots_hit')->unsigned();
+            $table->integer('mvps')->unsigned();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('csgo_profiles');
+    }
+}
