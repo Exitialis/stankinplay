ca5d5d2a1fe5e2ba061d1bb11dc987e2173de5ee
lebedev.a <lebedev.a@future-group.ru>
2016-10-14 14:25:45 +0300

    post request

--- app/Http/Controllers/Auth/RegistrationController.php
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\auth;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class RegistrationController extends Controller
@@ -14,6 +13,15 @@ class RegistrationController extends Controller
     }
 
     public function store(Request $request){
-
+        $this->validate($request,[
+            'login' => 'required|max:255',
+            'email' => 'required|email',
+            'pass' => 'required|max:255',
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'middle_name' => 'required|max:255',
+            'group' => 'required|max:255'
+        ]);
     }
+    
 }
