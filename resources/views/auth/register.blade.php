f67d061f15d5f056fcb8f5af130486aa8555c767
Exitialis <lpexitialis@gmail.com>
2016-10-14 13:54:27 +0300

    clearing

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: exitialis
+ * Date: 14.10.16
+ * Time: 13:40
+ */
---
+++ resources/views/vendor/index.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+</head>
+<body>
+
+</body>
+</html>
