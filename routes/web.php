2e050147a7fff6b837f591feb64ef3a565dda2be
lebedev.a <lebedev.a@future-group.ru>
2016-10-14 13:51:47 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,16 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
+use Illuminate\Http\Request;
 
 Route::get('home', function() {
 	return view('test');
+});
+
+Route::group(['midlleware => web'], function (){
+	
+	Route::get('/auth/registration', 'Auth/RegistrationController@index');
+	
+	Route::post('/auth/registration', 'Auth/RegistrationController@store');
+	
 });
