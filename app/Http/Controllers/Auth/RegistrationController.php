dba1c485639fc264403928ccf0facb2b97f6cf0b
lebedev.a <lebedev.a@future-group.ru>
2016-10-15 12:25:10 +0300

    Correct validate

--- app/Http/Controllers/Auth/RegistrationController.php
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\auth;
 
+use App\Http\Requests\StoreRegistrationPost;
 use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
@@ -12,16 +13,8 @@ class RegistrationController extends Controller
         return view('auth.registration');
     }
 
-    public function store(Request $request){
-        $this->validate($request,[
-            'login' => 'required|max:255',
-            'email' => 'required|email',
-            'pass' => 'required|max:255',
-            'first_name' => 'required|max:255',
-            'last_name' => 'required|max:255',
-            'middle_name' => 'required|max:255',
-            'group' => 'required|max:255'
-        ]);
+    public function store(StoreRegistrationPost $request){
+        
     }
     
 }
