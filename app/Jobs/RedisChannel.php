076b27b8fd350bf937a5411f5aefe6a549e32812
Exitialis <lpexitialis@gmail.com>
2016-10-17 12:25:14 +0300

    тесты

---
+++ app/Jobs/RedisChannel.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Log;
+use Redis;
+
+class RedisChannel implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Redis::publish('steambot', 'start');
+
+        Redis::subscribe(['laravel'], function($message) {
+            Log::info($message);
+        });
+    }
+}
