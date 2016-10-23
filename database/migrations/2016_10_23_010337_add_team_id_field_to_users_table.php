75c224179d5f4ab5b141fbdeee1f83395f0b0b20
Exitialis <lpexitialis@gmail.com>
2016-10-23 16:10:22 +0300

    Fix migration problem

--- database/migrations/2016_10_23_010337_add_team_id_field_to_users_table.php
+++ database/migrations/2016_10_23_010337_add_team_id_field_to_users_table.php
@@ -27,6 +27,7 @@ class AddTeamIdFieldToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function(Blueprint $table) {
+            $table->dropForeign('users_team_id_foreign');
             $table->dropColumn('team_id');
         });
     }
