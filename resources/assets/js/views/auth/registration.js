fd516121cfb1842ae530cf52ca32e06d0245320d
Exitialis <lpexitialis@gmail.com>
2016-10-25 21:00:27 +0300

    Фиксы

--- resources/assets/js/views/auth/registration.js
+++ resources/assets/js/views/auth/registration.js
@@ -28,15 +28,12 @@ Vue.component('registration', {
                 this.loading = false;
                 this.confirm = true;
                 this.errors = {};
-                localStorage.setItem('registration', true);
             }).catch(response => {
                 this.loading = false;
-                localStorage.setItem('registration', false);
                 if (response.status === 422) {
                     this.errors = JSON.parse(response.body);
                     window.toastr.error('При регистрации произошла ошибка.')
                 }
-
             })
         },
         getDisciplines() {
@@ -54,6 +51,5 @@ Vue.component('registration', {
 
     mounted() {
         this.getDisciplines();
-        //this.registration = JSON.parse(localStorage.getItem('registration'));
     }
 });
